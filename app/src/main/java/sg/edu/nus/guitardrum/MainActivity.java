b8dfb3f2491c9917702fa9fa88dabcd49f66e593
delvinlow <delvinlowzy@gmail.com>
2017-02-16 01:02:07 +0800

    Program accelerometer for smashing action

--- app/src/main/java/sg/edu/nus/guitardrum/MainActivity.java
+++ app/src/main/java/sg/edu/nus/guitardrum/MainActivity.java
@@ -1,13 +1,64 @@
 package sg.edu.nus.guitardrum;
 
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements SensorEventListener {
+
+    private SensorManager mSensorManager;
+    private Sensor mAccelerometer;
+    private MediaPlayer mPlayer;
+    private final static double ACC = 28;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        mAccelerometer = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+
+        mPlayer = MediaPlayer.create(this, R.raw.cymbal);
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mSensorManager.registerListener(this, mAccelerometer, SensorManager.SENSOR_DELAY_FASTEST);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+//        mPlayer.release();
+        mSensorManager.unregisterListener(this);
+
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        int sensorType = event.sensor.getType();
+        float[] values = event.values;
+
+        if (sensorType == Sensor.TYPE_ACCELEROMETER) {
+            if (values[2] > ACC) {
+                Log.i("sensor", "running");
+                Log.e("Accelerometer Triggered", " " + values[0] + " " + values[1] + " " + values[2]);
+                mPlayer.start();
+            }
+        }
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
     }
 }
