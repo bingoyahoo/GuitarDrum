c4ce3cce23e18a61a7dd47eaf880df85f4b8bc4c
LookRain <brantscube@hotmail.com>
2017-03-21 03:02:08 +0800

    added ripple animation dependency

--- app/src/main/java/sg/edu/nus/guitardrum/GuitarActivity.java
+++ app/src/main/java/sg/edu/nus/guitardrum/GuitarActivity.java
@@ -2,6 +2,10 @@ package sg.edu.nus.guitardrum;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+import com.skyfishjy.library.*;
 
 public class GuitarActivity extends AppCompatActivity {
 
@@ -9,5 +13,19 @@ public class GuitarActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_guitar);
+
+        /*
+            Make sure to Sync Project With Gradle Files (Tools -> Android -> Sync Project With Gradle Files) and rebuild the app
+            for usage of RippleBackground refer to: https://github.com/skyfishjy/android-ripple-background
+         */
+        final RippleBackground rippleBackground=(RippleBackground)findViewById(R.id.content);
+        ImageView imageView=(ImageView)findViewById(R.id.centerImage);
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                rippleBackground.startRippleAnimation();
+            }
+        });
+
     }
 }
