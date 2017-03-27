f0d9b1c50fe6312807d05f7986932f0c12fd1849
delvinlow <delvinlowzy@gmail.com>
2017-03-27 15:45:03 +0800

    Delete old activities, move buffer code into activities

--- app/src/main/java/sg/edu/nus/guitardrum/GuitarActivity.java
+++ app/src/main/java/sg/edu/nus/guitardrum/GuitarActivity.java
@@ -2,30 +2,72 @@ package sg.edu.nus.guitardrum;
 
 import android.animation.ArgbEvaluator;
 import android.animation.ValueAnimator;
+import android.content.Context;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.graphics.drawable.Drawable;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Handler;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.RelativeLayout;
+import android.widget.Toast;
 
 import com.skyfishjy.library.*;
+
+import java.util.Arrays;
 
 /*
             Make sure to Sync Project With Gradle Files (Tools -> Android -> Sync Project With Gradle Files) and rebuild the app
             for usage of RippleBackground refer to: https://github.com/skyfishjy/android-ripple-background
 */
 
-public class GuitarActivity extends AppCompatActivity {
+public class GuitarActivity extends AppCompatActivity implements SensorEventListener {
+
+
+    //variables for checking sampling rate
+    private final static long sampling_interval = 10000000; //ns
+    private final static long sampling_interval_error_margin = 2000000;//20%
+    private double last_x;
+    private double last_y;
+    private double last_z;
+    private long lastTimeStamp;
+    //Buffer variables
+    private boolean bufferisReady = false;
+    private double[][] buffer;
+    private double[][] nextBuffer;
+    private final static int bufferLen = 1024;
+    private final static int bufferOverlap = 512;
+    //    private final static int bufferLen = 64;
+//    private final static int bufferOverlap = 32;
+    private int bufferIndex;
+
+    private SensorManager mSensorManager;
+    private Sensor mAccelerometer;
+    private FeaturesExtractor featureExtractor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_guitar);
+
+        // For playing guitar sounds when acceleration detected
+        mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        mAccelerometer = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+
+        // For Buffers
+        buffer = new double[3][bufferLen];
+        nextBuffer = new double[3][bufferLen];
+        bufferIndex = 0;
+
+        featureExtractor = null;
 
         FloatingActionButton fab1 = (FloatingActionButton)findViewById(R.id.string_button_1);
         FloatingActionButton fab2 = (FloatingActionButton)findViewById(R.id.string_button_2);
@@ -156,5 +198,85 @@ public class GuitarActivity extends AppCompatActivity {
                 rb.stopRippleAnimation();
             }
         }, 1500);
+    }
+
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+    }
+
+    public void onSensorChanged(SensorEvent event) {
+        makeBuffer(event);
+        if (bufferisReady) {
+            doSomeCalculations(buffer);
+            bufferisReady = false;
+            //copy nextBuffer into buffer
+            for (int i = 0; i < bufferOverlap; i++) {
+                for (int j = 0; j < 3; j++) {
+                    buffer[j][i] = nextBuffer[j][i];
+                }
+            }
+        }
+    }
+
+
+    private void makeBuffer(SensorEvent event){
+        int sensor = event.sensor.getType();
+        float[] values = event.values;
+
+        if(sensor == Sensor.TYPE_ACCELEROMETER) {
+            if(lastTimeStamp == 0){
+                lastTimeStamp = event.timestamp;
+            }
+            else {
+                long timeInterval = event.timestamp - lastTimeStamp;
+                buffer[0][bufferIndex] = values[0];
+                buffer[1][bufferIndex] = values[1];
+                buffer[2][bufferIndex] = values[2];
+                last_x = values[0];
+                last_y = values[1];
+                last_z = values[2];
+                lastTimeStamp = event.timestamp;
+                bufferIndex += 1;
+                //buffer is full
+                if(bufferIndex == bufferLen){
+                    bufferisReady = true;
+                    //copy values into new buffer
+                    for(int i = 0; i < bufferOverlap; i++){
+                        for(int j=0; j<3; j++){
+                            nextBuffer[j][i] = buffer[j][i + bufferOverlap];
+                        }
+                    }
+                    bufferIndex = bufferOverlap;
+                }
+
+            }
+        }
+    }
+
+    private void doSomeCalculations(double[][] buffer){
+        try {
+            Log.v("Buffer is ", Arrays.toString(buffer[0]));
+            featureExtractor = new FeaturesExtractor(buffer[0], 100);
+        } catch (Exception e) {
+            Toast.makeText(this, "FeaturesExtractor cannot launch", Toast.LENGTH_SHORT).show();
+        }
+
+        try {
+            featureExtractor.calculateFeatuers();
+//            d.calculateFeatuersMean();
+        } catch (Exception e) {
+            Toast.makeText(this, "Features problem", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mSensorManager.registerListener(this, mAccelerometer, SensorManager.SENSOR_DELAY_FASTEST);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mSensorManager.unregisterListener(this);
     }
 }
--- app/src/main/java/sg/edu/nus/guitardrum/HomeActivity.java
+++ app/src/main/java/sg/edu/nus/guitardrum/HomeActivity.java
@@ -18,36 +18,13 @@ import android.widget.Toast;
 
 import java.util.Arrays;
 
-public class HomeActivity extends AppCompatActivity  implements SensorEventListener {
+public class HomeActivity extends AppCompatActivity  {
 
-    //variables for checking sampling rate
-    private final static long sampling_interval = 10000000; //ns
-    private final static long sampling_interval_error_margin = 2000000;//20%
-    private double last_x;
-    private double last_y;
-    private double last_z;
-    private long lastTimeStamp;
-    //Buffer variables
-    private boolean bufferisReady = false;
-    private double[][] buffer;
-    private double[][] nextBuffer;
-    private final static int bufferLen = 1024;
-    private final static int bufferOverlap = 512;
-//    private final static int bufferLen = 64;
-//    private final static int bufferOverlap = 32;
-    private int bufferIndex;
-
-    private SensorManager mSensorManager;
-    private Sensor mAccelerometer;
-    private FeaturesExtractor featureExtractor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
-        // For playing guitar sounds when acceleration detected
-        mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
-        mAccelerometer = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
 
         final ImageButton guitarButton = (ImageButton)findViewById(R.id.imageButton_home_guitar);
         final ImageButton drumButton = (ImageButton)findViewById(R.id.imageButton_home_drum);
@@ -60,13 +37,6 @@ public class HomeActivity extends AppCompatActivity  implements SensorEventListe
         final MediaPlayer guitarPlayer = MediaPlayer.create(this, R.raw.guitar_intro);
         final MediaPlayer drumPlayer = MediaPlayer.create(this, R.raw.drum_intro);
 
-
-        // For Buffers
-        buffer = new double[3][bufferLen];
-        nextBuffer = new double[3][bufferLen];
-        bufferIndex = 0;
-
-        featureExtractor = null;
 
         /*
             To be added:
@@ -141,125 +111,4 @@ public class HomeActivity extends AppCompatActivity  implements SensorEventListe
             }
         });
     }
-
-    public void onAccuracyChanged(Sensor sensor, int accuracy) {
-    }
-
-    public void onSensorChanged(SensorEvent event) {
-        makeBuffer(event);
-        if (bufferisReady) {
-            doSomeCalculations(buffer);
-            bufferisReady = false;
-            //copy nextBuffer into buffer
-            for (int i = 0; i < bufferOverlap; i++) {
-                for (int j = 0; j < 3; j++) {
-                    buffer[j][i] = nextBuffer[j][i];
-                }
-            }
-        }
-    }
-
-
-    private void makeBuffer(SensorEvent event){
-        int sensor = event.sensor.getType();
-        float[] values = event.values;
-
-        if(sensor == Sensor.TYPE_ACCELEROMETER) {
-            if(lastTimeStamp == 0){
-                lastTimeStamp = event.timestamp;
-            }
-            else {
-//                Log.v("Accelerometer x", String.valueOf(values[0]));
-//                Log.v("Accelerometer y", String.valueOf(values[1]));
-//                Log.v("Accelerometer z", String.valueOf(values[2]));
-                long timeInterval = event.timestamp - lastTimeStamp;
-                //Log.d("timestamp", "timediff " + timeInterval);
-                //Put values into buffer
-//                if(timeInterval > sampling_interval + 2 * sampling_interval_error_margin){
-//                    //Will this happen?
-////                    Log.d("buffer", "timediff is twice error margin" + timeInterval);
-//                }
-//                if(timeInterval > sampling_interval + sampling_interval_error_margin){
-//                    //If timestamp > sampling rate
-//                    //interpolate values
-                    //TODO: why is this not working?
-//                    long ratio = timeInterval / sampling_interval;
-//                    buffer[0][bufferIndex]= last_x + (values[0] - last_x) * ratio;
-//                    buffer[1][bufferIndex] = last_y + (values[1] - last_y) * ratio;
-//                    buffer[2][bufferIndex] = last_z + (values[2] - last_z) * ratio;
-//                    last_x = buffer[0][bufferIndex];
-//                    last_y = buffer[1][bufferIndex];
-//                    last_z = buffer[2][bufferIndex];
-//                    lastTimeStamp = lastTimeStamp + sampling_interval;
-//                }
-//                else if (timeInterval < sampling_interval - sampling_interval_error_margin){
-//                    //don't do anything
-//                    //Not sure if we should do this, or extrapolate values?
-//                }
-//                else {
-//                    buffer[0][bufferIndex] = values[0];
-//                    buffer[1][bufferIndex] = values[1];
-//                    buffer[2][bufferIndex] = values[2];
-//                    last_x = values[0];
-//                    last_y = values[1];
-//                    last_z = values[2];
-//                    lastTimeStamp = event.timestamp;
-//                }
-                buffer[0][bufferIndex] = values[0];
-                buffer[1][bufferIndex] = values[1];
-                buffer[2][bufferIndex] = values[2];
-                last_x = values[0];
-                last_y = values[1];
-                last_z = values[2];
-                lastTimeStamp = event.timestamp;
-                bufferIndex += 1;
-                //buffer is full
-                if(bufferIndex == bufferLen){
-                    bufferisReady = true;
-                    //copy values into new buffer
-                    for(int i = 0; i < bufferOverlap; i++){
-                        for(int j=0; j<3; j++){
-                            nextBuffer[j][i] = buffer[j][i + bufferOverlap];
-                        }
-                    }
-                    bufferIndex = bufferOverlap;
-                }
-
-            }
-        }
-    }
-
-    private void doSomeCalculations(double[][] buffer){
-//        Log.v("Calculating", " features");
-        //dummy function
-        try {
-            Log.v("Buffer is ", Arrays.toString(buffer[0]));
-            featureExtractor = new FeaturesExtractor(buffer[0], 100);
-        } catch (Exception e) {
-            Toast.makeText(this, "FeaturesExtractor cannot launch", Toast.LENGTH_SHORT).show();
-        }
-
-        try {
-            featureExtractor.calculateFeatuers();
-//            d.calculateFeatuersMean();
-        } catch (Exception e) {
-            Toast.makeText(this, "Features problem", Toast.LENGTH_SHORT).show();
-        }
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-        mSensorManager.registerListener(this, mAccelerometer, SensorManager.SENSOR_DELAY_FASTEST);
-    }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-        mSensorManager.unregisterListener(this);
-    }
-
-
-
-
 }
